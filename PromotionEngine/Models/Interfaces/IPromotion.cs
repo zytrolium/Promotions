1e591cc44e0af3030c6b1bedb949d6a9bf051f64
zytrolium <hux.jan@gmail.com>
2021-09-28 14:33:55 +0200

    Added interface of promotion object

---
+++ PromotionEngine/Models/Interfaces/IPromotion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models.Interfaces
+{
+    public interface IPromotion<T>
+    {
+        List<IItem<T>> GetItemList();
+        double GetPrice();
+    }
+}
