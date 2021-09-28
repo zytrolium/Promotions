ea8d18320ab465792bce1e92489dd5aa237a88dd
zytrolium <hux.jan@gmail.com>
2021-09-28 15:01:45 +0200

    Implementation of CharPromotion

---
+++ PromotionEngine/Models/Promotion.cs
@@ -0,0 +1,34 @@
+﻿using PromotionEngine.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+    public class CharPromotion : IPromotion<char>
+    {
+        private List<IItem<char>> _itemList;
+        private double _price;
+        public CharPromotion(List<IItem<char>> ItemList, double Price)
+        {
+            if (ItemList == null)
+            {
+                throw new ArgumentNullException("ItemList", "ItemList cannot be null");
+            }
+            _itemList = ItemList;
+            _price = Price;
+        }
+
+        public List<IItem<char>> GetItemList()
+        {
+            return _itemList;    
+        }
+
+        public double GetPrice()
+        {
+            return _price;
+        }
+    }
+}
