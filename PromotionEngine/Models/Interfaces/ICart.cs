887d62811cc6d6be9c59dd0106bb6e2449f1ad6f
zytrolium <hux.jan@gmail.com>
2021-09-28 14:35:13 +0200

    Added Interface for the Cart objects

---
+++ PromotionEngine/Models/Interfaces/ICart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models.Interfaces
+{
+    interface ICart<T>
+    {
+        void addItemToCart(IItem<T> Item);
+        List<IItem<T>> getItemsInCart();
+    }
+}
