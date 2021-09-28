21eefd0fbf8862454248f16aeb3855a5a9b1bef8
zytrolium <hux.jan@gmail.com>
2021-09-28 14:43:16 +0200

    Implementation of item where sku is only a single letter.

---
+++ PromotionEngine/Models/CharItem.cs
@@ -0,0 +1,33 @@
+﻿using PromotionEngine.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+    public class CharItem : IItem<char>
+    {
+        private char _sku;
+        private double _price;
+        public CharItem(char Sku, double Price)
+        {
+            if (!char.IsLetter(Sku))
+            {
+                throw new ArgumentException($"Only letter allowed for sku. Recieved Sku as: {Sku}", "Sku");
+            }
+            _sku = Sku;
+            _price = Price;
+        }
+        public double GetPrice()
+        {
+            return _price;
+        }
+
+        public char GetSku()
+        {
+            return _sku;
+        }
+    }
+}
